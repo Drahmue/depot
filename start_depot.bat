f2bff1466338619a3af9567aa641cb217024fb24
Drahmue <member@haunschild-family.de>
2025-09-01 09:43:23 +0200

    created start_depot.bat to start on server from task scheduler

---
+++ start_depot.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+pushd "%~dp0"
+
+set "LOGDIR=%CD%\logs"
+if not exist "%LOGDIR%" mkdir "%LOGDIR%"
+
+for /f %%I in ('powershell -NoProfile -Command "(Get-Date).ToString(\"yyyy-MM\")"') do set "LOGSTAMP=%%I"
+set "LOGFILE=%LOGDIR%\depot_!LOGSTAMP!.log"
+
+echo [%date% %time%] START Depot Script >> C:\ProgramData\bat_trace_depot.txt
+echo LOGFILE=!LOGFILE! >> C:\ProgramData\bat_trace_depot.txt
+
+set PYTHONIOENCODING=utf-8
+
+rem --- Main script execution ---
+"%CD%\.venv\Scripts\python.exe" -u "depot.py" >> "!LOGFILE!" 2>&1
+set "RC=!ERRORLEVEL!"
+echo [%date% %time%] ENDE Depot Script (ExitCode=!RC!) >> "!LOGFILE!"
+
+rem --- Clean up old log files (older than 120 days) ---
+powershell -NoProfile -Command ^
+  "Get-ChildItem -Path '%LOGDIR%' -Filter 'depot_*.log' | Where-Object LastWriteTime -lt (Get-Date).AddDays(-120) | Remove-Item -Force -ErrorAction SilentlyContinue"  >nul 2>&1
+
+popd
+exit /b !RC!
